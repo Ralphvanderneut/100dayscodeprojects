8f344f8dfadb7d92e538623f7bffec7d4f7d4392
Ralph van der Neut <ralph.vdn@live.nl>
2020-09-28 18:06:05 +0200

    created small test project

---
+++ codeweekprojects/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <TeleportMe></TeleportMe>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import TeleportMe from "./components/TeleportMe";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    TeleportMe,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ codeweekprojects/src/components/TeleportMe.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <input type="text" v-model="location" />
+    <input type="checkbox" v-model="disabled" />
+    <button @click="Move">move</button>
+    <teleport :to="teleport.to" :disabled="teleport.disabled">
+      <div class="teleport-me">teleportme to body</div>
+    </teleport>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      location: "",
+      disabled: false,
+      teleport: {
+        to: "body",
+        disabled: false,
+      },
+    };
+  },
+  methods: {
+    Move() {
+      this.teleport.to = this.location;
+      this.teleport.disabled = this.disabled;
+    },
+  },
+};
+</script>
+
+<style lang="css" scoped>
+.teleport-me {
+  background-color: #0000ff;
+  color: white;
+}
+</style>
